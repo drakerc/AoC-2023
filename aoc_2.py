184589fd5c07a4fb5339d7ed421ee554f3ecdd06
Krzysztof Kosinski <drakerc@me.com>
2023-12-02 11:12:16 +0100

    Update aoc_2.py

--- aoc_2.py
+++ aoc_2.py
@@ -33,8 +33,32 @@ def process_input() -> int:
     return sum_of_game_ids
 
 
+def process_input_part_2() -> int:
+    input_lines = get_input_lines()
+    power_of_colors = 0
+
+    for line in input_lines:
+        minimum_colors = {
+            "red": 1,
+            "green": 1,
+            "blue": 1
+        }
+        split_by_game_id = line.split(":")
+        games = split_by_game_id[1].split(";")
+        for game in games:
+            split_by_colors = game.split(",")
+            for split_by_color in split_by_colors:
+                split_by_numbers_and_colors = split_by_color.split()
+                if int(split_by_numbers_and_colors[0]) > minimum_colors[split_by_numbers_and_colors[1]]:
+                    minimum_colors[split_by_numbers_and_colors[1]] = int(split_by_numbers_and_colors[0])
+        power_of_color = minimum_colors["red"] * minimum_colors["green"] * minimum_colors["blue"]
+        power_of_colors = power_of_colors + power_of_color
+    return power_of_colors
+
+
 def main():
     print(process_input())
+    print(process_input_part_2())
 
 
 if __name__ == "__main__":
